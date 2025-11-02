53c79a27697ad9761663137f850a1ffb225eef60
Mário Carneiro <emailoftheyear@gmail.com>
2025-11-02 23:44:08 +0000

    Allow stopping autoplay

--- www/script.js
+++ www/script.js
@@ -1,5 +1,11 @@
 (function() {
   document.addEventListener("DOMContentLoaded", (event) => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has('silence')) {
+      document.getElementById('audio').removeAttribute('autoplay');
+    }
+
     document.body.addEventListener("click", (event) => {
       let audio = document.getElementById("audio");
 
